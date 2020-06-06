ff5757825577f7f738600619ceabdfb3d085ae37
scorchy <vkumarsaraswat@gmail.com>
2020-06-07 00:19:19 +0530

    unused lines and some warnings removed

--- lib/DrawerPages/your_orders_page.dart
+++ lib/DrawerPages/your_orders_page.dart
@@ -3,6 +3,10 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class YourOrders extends StatefulWidget {
+  final String phno;
+
+  const YourOrders({Key key, this.phno}) : super(key: key);
+
   @override
   _YourOrdersState createState() => _YourOrdersState();
 }
@@ -16,6 +20,7 @@ String t;
 
 class _YourOrdersState extends State<YourOrders> {
   void getOrderList() {
+    String user = "+91${widget.phno}";
     DatabaseReference dailyitemsref = FirebaseDatabase.instance
         .reference()
         .child('Orders')
@@ -124,19 +129,6 @@ class _YourOrdersState extends State<YourOrders> {
           onPressed: () => Navigator.pop(context, false),
         ),
       ),
-//        body: Column(
-//          children: [
-////            Text(pastOrders[0].d.length.toString()),
-//            Text(currOrders.length.toString()),
-////            Text(pastOrders.length.toString()),
-//            Text(currOrders[0].d.length.toString()),
-//            Text(currOrders[1].d.length.toString()),
-//            Text(currOrders[2].d.length.toString()),
-//            Text(currOrders[0].d[0].price.toString()),
-//            Text(currOrders[0].d[0].name.toString()),
-//
-//          ],
-//        )
       body: Column(
         children: <Widget>[
           Text('Current Orders'),
@@ -151,48 +143,7 @@ class _YourOrdersState extends State<YourOrders> {
           )
         ],
       ),
-//        body: currOrders.isEmpty
-//            ? pastOrders.isEmpty
-//                ?
-//                //When both the orders list are empty
-//                Container(
-//                    color: Colors.white,
-//                    child: Center(
-//                        child: Text('Your past orders will be displayed')),
-//                  )
-//                :
-//                //Current orders are empty but we have past pastOrders
-//                Column(
-//                    children: <Widget>[
-//                      Text('Past Orders'),
-////                      Text(pastOrders[0].d[0].name),
-////                      Text(pastOrders[0].d[0].price),
-//                    ],
-//                  )
-//            : pastOrders.isEmpty
-//                ?
-//                //We have only current orders
-//                Column(
-//                    children: <Widget>[
-//                      Text('Current Orders'),
-//                      Text(currOrders[0].d.length.toString()),
-//                      Text(currOrders[0].d[0].price),
-//                      Text(currOrders[0].d[1].price),
-//                    ],
-//                  )
-//                :
-//                //We have both current and past orders
-//                Column(
-//                    children: <Widget>[
-//                      Text('Current Orders'),
-//                      Text(currOrders.length.toString()),
-//                      Text(currOrders[0].d.length.toString()),
-//                      Text('Past Orders'),
-//                      Text(pastOrders.length.toString()),
-////
-//                      Text(pastOrders[0].d.length.toString()),
-////                      Text(pastOrders[0].d[0].price.toString()),
-//                    ],
+
     );
   }
 }
