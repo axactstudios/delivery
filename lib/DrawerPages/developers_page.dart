beaacac9be86d5138a403a91474fa8217eaa2469
scorchy <vkumarsaraswat@gmail.com>
2020-06-07 16:11:37 +0530

    some changes in developers page

--- lib/DrawerPages/developers_page.dart
+++ lib/DrawerPages/developers_page.dart
@@ -27,41 +27,26 @@ class _DevelopersState extends State<Developers> {
         ),
       ),
       body: Container(
+        color: Colors.white,
         child: Center(
           child: Column(children: [
             Padding(
               padding: const EdgeInsets.only(top: 20),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0)),
-                elevation: 10,
-                child: Image.asset('images/logo_axact.png'),
+              child: Image.asset(
+                'images/web-development.png',
+                height: 200,
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Axact Studios',
-                style: TextStyle(
-                    fontSize: 40,
-                    color: Color(0xFF345995),
-                    fontFamily: 'sf_pro',
-                    fontWeight: FontWeight.bold),
+              padding: EdgeInsets.all(5),
+              child: Image.asset(
+                'images/axact2.png',
+                height: 150,
+                colorBlendMode: BlendMode.colorBurn,
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: Text(
-                'Design.Code.Create',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Color(0xFF345995),
-                  fontFamily: 'sf_pro',
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
               child: Text(
                 'Got an idea that needs to enter the competitive world of apps and websites?',
                 style: TextStyle(
@@ -72,7 +57,7 @@ class _DevelopersState extends State<Developers> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 5),
               child: Text(
                 'Let Us Know',
                 style: TextStyle(
@@ -83,76 +68,40 @@ class _DevelopersState extends State<Developers> {
               ),
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 FlatButton(
                   onPressed: () {
                     FlutterOpenWhatsapp.sendSingleMessage(
                         "917060222315", "Hello");
                   },
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 20.0),
-                    child: Card(
-                      elevation: 7.0,
-                      color: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0)),
-                      child: Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: Column(
-                          children: <Widget>[
-                            Icon(
-                              FontAwesomeIcons.whatsapp,
-                              size: 40.0,
-                              color: Colors.green,
-                            ),
-                            SizedBox(
-                              height: 30.0,
-                            ),
-                            Text(
-                              "Chat with us",
-                              style: TextStyle(fontSize: 18.0),
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
-                        ),
+                  child: Card(
+                    elevation: 7.0,
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        children: <Widget>[
+                          Icon(
+                            FontAwesomeIcons.whatsapp,
+                            size: 40.0,
+                            color: Colors.green,
+                          ),
+                          SizedBox(
+                            height: 30.0,
+                          ),
+                          Text(
+                            "Contact Us Now",
+                            style: TextStyle(fontSize: 18.0),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
                       ),
                     ),
                   ),
                 ),
-                FlatButton(
-                  onPressed: () => Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Support())),
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 10.0),
-                    child: Card(
-                      elevation: 7.0,
-                      color: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0)),
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(
-                            vertical: 20.0, horizontal: 39.0),
-                        child: Column(
-                          children: <Widget>[
-                            Icon(
-                              FontAwesomeIcons.mailBulk,
-                              size: 40.0,
-                              color: Color.fromARGB(255, 143, 23, 255),
-                            ),
-                            SizedBox(
-                              height: 30.0,
-                            ),
-                            Text(
-                              "Email us",
-                              style: TextStyle(fontSize: 18.0),
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                )
               ],
             ),
           ]),
